7a4cb1c485da57ef872511a49eca086cb46a1211
krimovfirdavs <krimovfirdavs@gmail.com>
2026-02-10 15:45:04 +0500

    fix

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-change-in-prod
 
 DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ["45.138.159.194"]
 
 INSTALLED_APPS = [
     'django.contrib.auth',
